7e8ce1f3dc55b63493a9dfa9f3015c3f731d1379
Juliana Boudakian <julianal.b@hotmail.com>
2019-04-11 14:11:17 +0100

    routes

--- WEB/src/index.js
+++ WEB/src/index.js
@@ -1,27 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../src/App";
-import "../public/css/font-awesome.min.css";
-import "../public/css/bootstrap.css";
-import "../public/css/style.css";
-import "../public/css/responsive.css";
-import "../public/css/shortcuts.css";
-import "../public/css/plugin/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css";
-import "../public/css/plugin/bootstrap-select/bootstrap-select.css";
-import "../public/css/plugin/bootstrap-toggle/bootstrap-toggle.min.css";
-import "../public/css/plugin/bootstrap-wysihtml5/bootstrap-wysihtml5.css";
-import "../public/css/plugin/summernote/summernote.css";
-import "../public/css/plugin/summernote/summernote-bs3.css";
-import "../public/css/plugin/sweet-alert/sweet-alert.css";
-import "../public/css/plugin/datatables/datatables.css";
-import "../public/css/plugin/chartist/chartist.min.css";
-import "../public/css/plugin/rickshaw/rickshaw.css";
-import "../public/css/plugin/rickshaw/detail.css";
-import "../public/css/plugin/rickshaw/graph.css";
-import "../public/css/plugin/rickshaw/legend.css";
-import "../public/css/plugin/date-range-picker/daterangepicker-bs3.css";
-import "../public/css/plugin/fullcalendar/fullcalendar.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import "./css/font-awesome.min.css";
+import "./css/bootstrap.css";
+import "./css/style.css";
+import "./css/responsive.css";
+import "./css/shortcuts.css";
+import "./css/plugin/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css";
+import "./css/plugin/bootstrap-select/bootstrap-select.css";
+import "./css/plugin/bootstrap-toggle/bootstrap-toggle.min.css";
+import "./css/plugin/bootstrap-wysihtml5/bootstrap-wysihtml5.css";
+import "./css/plugin/summernote/summernote.css";
+import "./css/plugin/summernote/summernote-bs3.css";
+import "./css/plugin/sweet-alert/sweet-alert.css";
+import "./css/plugin/datatables/datatables.css";
+import "./css/plugin/chartist/chartist.min.css";
+import "./css/plugin/rickshaw/rickshaw.css";
+import "./css/plugin/rickshaw/detail.css";
+import "./css/plugin/rickshaw/graph.css";
+import "./css/plugin/rickshaw/legend.css";
+import "./css/plugin/date-range-picker/daterangepicker-bs3.css";
+import "./css/plugin/fullcalendar/fullcalendar.css";
+import "./fonts/fontawesome-webfont.eot";
+import "./fonts/fontawesome-webfont.svg";
+import "./fonts/fontawesome-webfont.ttf";
+import "./fonts/fontawesome-webfont.woff";
+import "./fonts/fontawesome-webfont.woff2";
 
+// import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
